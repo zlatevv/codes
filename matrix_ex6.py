2cdf06710928eee6f8f8e296f6d0224d995df87e
zlatevv <146636754+zlatevv@users.noreply.github.com>
2023-12-07 20:03:20 +0200

    Update matrix_ex6.py

--- matrix_ex6.py
+++ matrix_ex6.py
@@ -1,12 +1,12 @@
-row_col = int(input("Enter the number of collumns and rows:"))
+row_col = int(input())
 matrix = []
 for row in range(row_col):
     matrix.append([])
     for col in range(row_col):
         matrix[row].append(col + 1 + row * row_col)
-print(*matrix, sep = "\n")
 sum = 0
 for i in range(row_col):
-    for j in range(i, row_col):
-        sum += matrix[j][i]
+    for j in range(i+1):
+        sum += matrix[i][j]
 print(sum)
+print(*matrix, sep="\n")
