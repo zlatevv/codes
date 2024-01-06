a22c3f86e66ae98c04a58ac617b0fc9a97689117
zlatevv <146636754+zlatevv@users.noreply.github.com>
2024-01-06 13:20:02 +0200

    Add files via upload

---
+++ petshop.py
@@ -0,0 +1,6 @@
+dog_food_counter = int(input())
+cat_food_counter = int(input())
+dog_sum = dog_food_counter * 2.5
+cat_sum = cat_food_counter * 4
+total_sum = dog_sum + cat_sum
+print(f"{total_sum} lv.")
---
+++ projectcreation.py
@@ -0,0 +1,4 @@
+name = input()
+projects = int(input())
+time = 3 * projects
+print(f"The architect {name} will need {time} hours to complete {projects} project/s.")
